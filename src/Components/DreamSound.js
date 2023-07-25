868170ea9b2a06f76d83e0a814212651fdb2575d
SharukhGV <sharukhvelupillai@pursuit.org>
2023-07-24 22:58:26 -0400

    footer elements

--- src/Components/DreamSound.js
+++ src/Components/DreamSound.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import thedreamsound from './../../src/dreamsound.mp3';
+// import React from 'react';
+// import thedreamsound from './../../src/dreamsound.mp3';
 
-const DreamSound = () => {
-  return (
-    <div className='audioCard'>
-      <audio src={thedreamsound} type="audio/mp3" controls />
-    </div>
-  );
-}
+// const DreamSound = () => {
+//   return (
+//     <div className='audioCard'>
+//       <audio src={thedreamsound} type="audio/mp3" controls />
+//     </div>
+//   );
+// }
 
-export default DreamSound;
+// export default DreamSound;
